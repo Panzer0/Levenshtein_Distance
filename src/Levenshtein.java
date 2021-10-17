562470e50e988d9e4433408b307ecd836eb3f480
Daniel Wieczorek <daniel.pazur@gmail.com>
2021-10-17 17:26:48 +0200

    First steps

--- src/Levenshtein.java
+++ src/Levenshtein.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class Levenshtein {
+    public int calculateDistance(String word1, String word2) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String word1, word2;
+
+        System.out.println("Enter the first word");
+        word1 = scanner.nextLine();
+        System.out.println("Enter the first word");
+        word2 = scanner.nextLine();
+
+
+    }
 }
