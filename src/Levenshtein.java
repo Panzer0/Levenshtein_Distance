337056a16b6b411084a5044279ca8aa513cab4dc
Panzer0 <daniel.pazur@gmail.com>
2021-10-17 23:15:10 +0200

    Test push from a new device

--- src/Levenshtein.java
+++ src/Levenshtein.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Levenshtein {
-    public int calculateDistance(String word1, String word2) {
+    public static int calculateDistance(String word1, String word2) {
         return 0;
     }
 
@@ -14,6 +14,6 @@ public class Levenshtein {
         System.out.println("Enter the first word");
         word2 = scanner.nextLine();
 
-
+        System.out.println("The distance is " + calculateDistance(word1, word2));
     }
 }
